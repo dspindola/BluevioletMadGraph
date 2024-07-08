246fa3005c98c280861fd0f39dbee88e8d5363d7
no-turno <davispindola2828@gmail.com>
2024-07-08 00:44:53 -0300

    feat: Display editor preferences in theme app
    The theme app now fetches and displays the user's editor preferences. This allows users to see their current editor configuration within the theme app context.

--- src/theme/app.tsx
+++ src/theme/app.tsx
@@ -1,8 +1,50 @@
-import { Button } from "../../packages/components/button";
+import { useReplit } from "@replit/extensions-react";
+import type { EditorPreferences, } from "@replit/extensions";
+import { Suspense, use } from "react";
+
+function Preferences({ loader }: { loader: () => Promise<{ error: null | Error, data: EditorPreferences | null }> }) {
+    const { error, data } = use(loader())
+
+    if (error) {
+        return <div>
+            <h1>Error</h1>
+            <p>{error.message}</p>
+        </div>
+    }
+
+    return <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+}
 
 export function App() {
+    const { replit, error } = useReplit()
+
+    if (error) {
+        return <main className="min-h-screen bg-white dark:bg-black text-white">
+            <h1>theme app</h1>
+            <p>{error.message}</p>
+        </main>
+    }
+
+    const loadConfig = () => replit.experimental.editor.getPreferences()
+        .then(preferences => {
+            return {
+                error: null,
+                data: preferences
+            }
+        })
+        .catch(error => {
+            return {
+                error,
+                data: null
+            }
+        })
+
     return <main className="min-h-screen bg-white dark:bg-black text-white">
         <h1>theme app</h1>
-        <Button />
+        <Suspense>
+            <Preferences loader={loadConfig} />
+        </Suspense>
     </main>
 }
